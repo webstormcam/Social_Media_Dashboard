e11cc29273e38a5ec6847b500480ee015bca737c
Cameron Chandler <webstormcam@gmail.com>
2022-01-20 05:15:53 -0500

    Got the white color to come through

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let background = document.getElementsByTagName("BODY")[0];
 let toggle = document.getElementById('toggle');
 let social = document.getElementsByClassName("social")
+let white = document.getElementsByClassName('white')
 
 toggle.addEventListener('click',function(){
 
@@ -9,10 +10,21 @@ toggle.addEventListener('click',function(){
        for(var i = 0; i<social.length;i++){
            social[i].style.backgroundColor ='#252B42'
        }
+       for(var i =0; i<white.length;i++){
+           white[i].style.color ='#fff'
+       }
+
+
     } else{
         background.style.backgroundColor ='#fff'
         for(var i = 0; i<social.length;i++){
             social[i].style.backgroundColor ='hsl(227, 47%, 96%)'
         }
+
+        for(var i = 0; i<white.length;i++){
+            white[i].style.color ='initial'
+        }
+
+
     }
 })
